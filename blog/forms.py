9e80cb47786f3dbb980a7b94011f96852abf1c3f
hongildong <hong@sesac.com>
2025-07-31 18:02:41 +0900

    게시글 파일 첨부, 다운로드 기능

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from blog.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'uploaded_image' ]
+        fields = ['title', 'content', 'uploaded_image', 'uploaded_file' ]
 
 class PostForm1(forms.ModelForm):
     class Meta:
--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,8 @@ class Post(models.Model):
                                         null=True)
     uploaded_image = models.ImageField(upload_to='images/',
                                        blank=True)
-
+    uploaded_file = models.FileField(upload_to='files/',
+                                     blank=True)
     def __str__(self):
         return f'게시글제목: {self.title} - 게시글내용 - {self.content} - 생성시간 - {self.created_date} - 업데이트-{self.updated_date}'
     def get_absolute_url(self):
